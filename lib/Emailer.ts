f084c3276ab5e9073ebd9e546c13046e8531ef33
slanska <slan_ska@yahoo.com>
2016-10-10 23:40:24 -0400

    emai support

---
+++ lib/Emailer.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by slanska on 2016-10-10.
+ */
+
+/*
+ Helper class for composing emails for different users (based on their IDs)
+ and using different templates
+ */
+class Emailer
+{
+}
+
+export = Emailer;
