8c1810931ed35b0d5404e2b3975f88084073274f
slanska <slan_ska@yahoo.com>
2016-11-19 20:52:57 -0500

    UI based on Framework 7

--- public/app/app.ts
+++ public/app/app.ts
@@ -8,12 +8,12 @@ declare var Template7;
 declare var Dom7;
 declare var Framework7;
 
-// require('framework7');
 import Vue = require('vue');
+var Framework7Vue = require('framework7-vue');
+
+Vue.use(Framework7Vue);
 
 import _ = require('lodash');
-
-console.log(Vue, _);
 
 var isAndroid = Framework7.prototype.device.android === true;
 var isIos = Framework7.prototype.device.ios === true;
@@ -33,6 +33,42 @@ if (!isIos)
     $$('.view .navbar').prependTo('.view .page');
 }
 
+// Init Vue App
+var app = new Vue({
+    // Root Element
+    el: '#app',
+    // Framework7 Parameters
+    framework7: {
+        root: '#app', //Should be same as app el
+        animateNavBackIcon: true,
+        routes: Routes,
+    },
+    // Custom App Data
+    data: function ()
+    {
+        return {
+            user: {
+                name: 'Vladimir',
+                lastName: 'Kharlampidi',
+                age: 30
+            },
+            popupOpened: false,
+            loginScreenOpened: false,
+            pickerOpened: false,
+            actionsOpened: false
+        };
+    },
+    // Custom App Methods
+    methods: {
+        onF7Init: ()=>
+        {
+            debugger;
+            console.log('f7-init');
+
+        }
+    }
+});
+
 // Init App
 export var nauth2App = new Framework7({
     // Enable Material theme for Android device only
@@ -46,10 +82,11 @@ export var nauth2App = new Framework7({
 export var nauth2MainView = nauth2App.addView('.view-main', {
     // Material doesn't support it but don't worry about it
     // F7 will ignore it for Material theme
-    dynamicNavbar: true
-});
+    dynamicNavbar: true,
 
-// create app
+    //Needed to enable navigation between inline pages
+    domCache: true
+});
 
 // set scripts based on client
 
@@ -60,8 +97,8 @@ export var nauth2MainView = nauth2App.addView('.view-main', {
         Dom7('head').append(
             '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.material.min.css">' +
             '<link rel="stylesheet" href="public/bower_components/Framework7/dist/css/framework7.material.colors.min.css">'
-            // public/bower_components/Framework7/dist/css/framework7.material.rtl.min.css
             + '<link rel="stylesheet" href="css/style.css">'
+            + ' <link rel="stylesheet" href="./bower_components/framework7-icons/css/framework7-icons.css">'
         );
     }
     else
@@ -69,11 +106,10 @@ export var nauth2MainView = nauth2App.addView('.view-main', {
         Dom7('head').append(
             '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.ios.min.css">' +
             '<link rel="stylesheet" href="bower_components/Framework7/dist/css/framework7.ios.colors.min.css">'
-            // public/bower_components/Framework7/dist/css/framework7.ios.rtl.min.css
             +
             '<link rel="stylesheet" href="css/style.css">'
+            +
+            '<link rel="stylesheet" href="./bower_components/framework7-icons/css/framework7-icons.css">'
         );
     }
 })();
-
-//
